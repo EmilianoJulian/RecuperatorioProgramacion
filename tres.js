9c48106b08d51f7f5a181793b9d2a6e8c2dc5ebe
Keisuke <keisuke.julian.2003@gmail.com>
2021-03-10 21:11:42 -0300

    cuarto commit

--- tres.js
+++ tres.js
@@ -4,10 +4,34 @@ function mostrar()
 	var nombreCliente;
 	var cantidadLamparas;
 	var marcaLampara;
-	var precioLampara;
+	var precioTotalLamparas;
 	var descuentoFelipe;//Si supera las 5 lamparas 10%descuento
 	var descuentoArgentina;//Si la marca es ArgentinaLuz y compra 3 o mas unidades, el descuento es del 5%.
 	var descuentoIlluminatis;
+	var contadorCantidadDeVentas;
+	//A
+	var recaudoDeLaEmpresa;
+	var acumuladorPrecioTotalLampara;
+	//B
+	var acumuladorPerdidaEmpresa;
+	switch (marca)
+	{
+		case "felipeLamparas":
+			if (cantidadLamparas>5)
+			{
+				descuentoFelipe=0.90;
+			}
+			break;
+		case "argentinaLuz":
+			if (cantidadLamparas>2)
+			{
+				descuentoArgentina=0.95;				
+			}
+			break;
+		case "illuminatis":
+			descuentoIlluminatis=1;
+			break;
+	}
 
 	do
 	{
@@ -29,36 +53,34 @@ function mostrar()
 			marcaLampara=parseInt("ERROR!!!. ingrese marca de la lampara. (felipeLamparas - argentinaLuz - illuminatis)");
 		}
 
-		precioLampara=parseInt(prompt("Ingrese el precio de la lampara"));
-		while (isNaN(precioLampara))
+		precioTotalLamparas=parseInt(prompt("Ingrese el precio de la lampara"));
+		while (isNaN(precioTotalLamparas))
 		{
-			precioLampara=parseInt(prompt("ERROR!!!. Ingrese el precio de la lampara"));	
+			precioTotalLamparas=parseInt(prompt("ERROR!!!. Ingrese el precio de la lampara"));	
 		}
-	
+		//Fin pedir datos
+
+		contadorCantidadDeVentas++;
+
+		//A
+		acumuladorPrecioTotalLampara=acumuladorPrecioTotalLampara+precioTotalLamparas;
+		recaudoDeLaEmpresa=acumuladorPrecioTotalLampara;
+		//B
+		acumuladorPerdidaEmpresa=
+
+		
+
 		continuar=confirm("Desea reingresar datos?")
 	}while(continuar);
-	//Fin pedir datos
+	
 
-	switch (marca)
-	{
-		case "felipeLamparas":
-			if (cantidadLamparas>5)
-			{
-				descuentoFelipe=0.90;
-			}
-			break;
-		case "argentinaLuz":
-			if (cantidadLamparas>2)
-			{
-			descuentoArgentina=0.95;				
-			}
-			break;
-		case "illuminatis":
-			descuentoIlluminatis=1;
-			break;
-	}
+	
 
 	//A
+	document.write("El recaudo del la empresa fue "+recaudoDeLaEmpresa+" en "+ contadorCantidadDeVentas+" compras");
+	//B
+
+	
 
 
 
